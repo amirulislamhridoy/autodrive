dc6a1f3209dffbddf74022c9675b3c3e927c479b
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-12-25 21:13:52 +0600

    ranking background fixed

--- components/LandingPage/Ranking.js
+++ components/LandingPage/Ranking.js
@@ -1,6 +1,6 @@
 const Ranking = () => {
     return (
-        <section className='mx-auto max-w-7xl flex justify-around text-white py-16 bg-no-repeat' style={{background: `linear-gradient(rgba(10, 25, 49, 0.5), rgba(10, 25, 49, 0.7)), url('https://kitnew.moxcreative.com/mobirentz/wp-content/uploads/sites/28/2022/12/classic-cars.jpg')`}}>
+        <section className='mx-auto max-w-7xl flex justify-around text-white py-16' style={{background: `linear-gradient(rgba(10, 25, 49, 0.5), rgba(10, 25, 49, 0.7)), url('https://kitnew.moxcreative.com/mobirentz/wp-content/uploads/sites/28/2022/12/classic-cars.jpg')`, backgroundAttachment: 'fixed', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
             <div className='text-center'>
                 <h1 className="text-5xl font-bold">68<span className='text-[#ffc947]'>+</span></h1>
                 <p className=''>Brands of Car</p>
