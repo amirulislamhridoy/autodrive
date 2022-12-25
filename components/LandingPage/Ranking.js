9c5cfa23ec1f0b795532be9860d14429c816bb7c
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-12-25 16:00:59 +0600

    randking component start

---
+++ components/LandingPage/Ranking.js
@@ -0,0 +1,24 @@
+const Ranking = () => {
+    return (
+        <section>
+            <div>
+                <h1>68<span>+</span></h1>
+                <p>Brands of Car</p>
+            </div>
+            <div>
+                <h1>96<span>%</span></h1>
+                <p>Happy Clients</p>
+            </div>
+            <div>
+                <h1>10<span>+</span></h1>
+                <p>Locations</p>
+            </div>
+            <div>
+                <h1>15<span>+</span></h1>
+                <p>Years of Experience</p>
+            </div>
+        </section>
+    );
+};
+
+export default Ranking;
