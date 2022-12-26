43ae8a637bae5af75501c1b7ee136ea8564eb690
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-12-27 01:38:59 +0600

    login responsive

--- pages/login.js
+++ pages/login.js
@@ -9,22 +9,22 @@ const Login = () => {
             <Head><title>Login</title></Head>
             <Navbar>login</Navbar>
 
-            <div className='mx-auto max-w-7xl flex justify-between mt-32'>
-                <div className='w-5/12 p-7 pt-10 border rounded-lg'>
+            <div className='mx-1 xl:mx-auto max-w-7xl flex justify-center lg:justify-between items-center mt-4 sm:mt-8 lg:mt-14 xl:mt-20 2xl:mt-32 mb-3'>
+                <div className='mg:w-6/12 xl:w-5/12 p-4 md:p-7 md:pt-10 border rounded-lg'>
                     <img className='bg-[#bbbbbb] mb-4' src='https://templatekits.themewarrior.com/autodrive/wp-content/uploads/sites/42/2021/12/logo-autodrive.png' alt=''></img>
                     <h2 className='text-2xl font-semibold mb-2.5'>Welcome back</h2>
                     <p className='text-sm text-[#aaaaaa]'>Start your website in seconds. Don&apos;t have an account? <a className='text-[#2c63ec] font-semibold' href='' alt=''>Sign up</a></p>
-                    <form className='mt-6'>
-                        <div className='flex justify-between'>
+                    <form className='mt-4 lg:mt-6'>
+                        <div className='lg:flex justify-between'>
                             <div>
                                 <label className='' htmlFor="email">Email</label>
                                 <br />
-                                <input id='email' className='mt-2 px-3 py-2 border border-2 rounded-lg' type='email' placeholder='user@example.com' required></input>
+                                <input id='email' className='lg:mt-2 px-3 py-2 border border-2 rounded-lg w-full' type='email' placeholder='user@example.com' required></input>
                             </div>
-                            <div>
+                            <div className='mt-2 lg:mt-0'>
                                 <label className='' htmlFor="password" required>Password</label>
                                 <br />
-                                <input id='password' className='mt-2 px-3 py-2 border border-2 rounded-lg' type='password' placeholder="******"></input>
+                                <input id='password' className='lg:mt-2 px-3 py-2 border border-2 rounded-lg w-full' type='password' placeholder="******"></input>
                             </div>
                         </div>
                         <div className='flex my-6 justify-center items-center gap-x-5'>
@@ -44,7 +44,7 @@ const Login = () => {
                         <button type='submit' disabled={!toggle} className={`w-full border rounded-lg py-2 ${toggle ? 'bg-[#2c63ec] text-white': " text-gray-400" }`}>Sign in to your account</button>
                     </form>
                 </div>
-                <div>
+                <div className='hidden lg:block'>
                     <img className="w-full" src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/authentication/illustration.svg' alt=''></img>
                 </div>
             </div>
