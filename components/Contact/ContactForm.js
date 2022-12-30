75af5fb43f72440f4ee09f9404200f1e4bc6b62e
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2022-12-31 04:14:32 +0600

    contact us from

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
-
 const ContactForm = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
@@ -8,18 +7,18 @@ const ContactForm = () => {
     };
     return (
         <section className='max-w-7xl mx-auto'>
-            <div>
-                <p className='text-[#bbbbbb] text-md sm:text-xl font-bold'>CONTACT US</p>
-                <h2 className='text-2xl font-bold sm:text-4xl sm:mt-2 md:mt-4'>GET IN TOUCH</h2>
-                <article className='mt-1 sm:mt-4 md:mt-8 text-justify'>
-                    Odio eu viverra tincidunt tristique ullamcorper blandit dipiscing nunc risus integer at elementum cursus. Lorem justo a felis elit amet.
-                </article>
-                <form onSubmit={handleSubmit(onSubmit)} className='p-5 bg-red-500'>
-                    <input className='focus:outline-0 p-5 w-full mb-1' {...register("name", { required: true })} aria-invalid={errors.name ? "true" : "false"} placeholder='Your Name'/>
-                    {errors.name?.type === 'required' && <p>You should must write name.</p>}
+            <p className='text-[#bbbbbb] text-md sm:text-xl font-bold mt-10'>CONTACT US</p>
+            <h2 className='text-2xl font-bold sm:text-4xl'>GET IN TOUCH</h2>
+            <article className='mt-1 sm:mt-2 text-justify'>
+                Odio eu viverra tincidunt tristique ullamcorper blandit dipiscing nunc risus integer at elementum cursus. Lorem justo a felis elit amet.
+            </article>
+            <div className='flex gap-x-12'>
+                <form className='p-7 bg-[#0a1931] mt-5 flex-1' onSubmit={handleSubmit(onSubmit)}>
+                    <input className='focus:outline-0 p-5 w-full mb-1' {...register("name", { required: true })} aria-invalid={errors.name ? "true" : "false"} placeholder='Your Name' />
+                    {errors.name?.type === 'required' && <p style={{ color: '#da635d' }}>You should must write name.</p>}
                     <input className='focus:outline-0 p-5 w-full my-1' {...register("email", { required: true, pattern: /^\S+@\S+\.\S+$/ })} placeholder='example@gmail.com' />
-                    {errors.email?.type === 'required' && <p>You should must write email.</p>}
-                    {errors.email?.type === 'pattern' && <p>Your email is invalid.</p>}
+                    {errors.email?.type === 'required' && <p style={{ color: '#da635d' }}>You should must write email.</p>}
+                    {errors.email?.type === 'pattern' && <p style={{ color: '#da635d' }}>Your email is invalid.</p>}
                     <input className='focus:outline-0 p-5 w-full my-1' type="number" {...register("number", {
                         required: {
                             value: true,
@@ -30,12 +29,12 @@ const ContactForm = () => {
                             message: 'Your number will be minium 4 digit.' // JS only: <p>error message</p> TS only support string
                         }
                     })} placeholder='0155555555' />
-                    {errors?.number?.message && <p>{errors?.number?.message}</p>}
-                    <textarea className='focus:outline-0 p-5 w-full min-h-[150px] mt-1'
+                    {errors?.number?.message && <p style={{ color: '#da635d' }}>{errors?.number?.message}</p>}
+                    <textarea className='focus:outline-0 p-5 w-full min-h-[120px] mt-1'
                         {...register("message", {
                             required: {
                                 value: true,
-                                message: 'You should write your text or message.'
+                                message: 'You should write text or message.'
                             },
                             minLength: {
                                 value: 10,
@@ -44,9 +43,26 @@ const ContactForm = () => {
                         })}
                         placeholder='Write Your Message'
                     />
-                    {errors?.message?.message && <p>{errors?.message?.message}</p>}
-                    <input type="submit" />
+                    {errors?.message?.message && <p style={{ color: '#da635d' }}>{errors?.message?.message}</p>}
+                    <input className='bg-[#ffc947] py-3 px-8 font-bold mt-2' type="submit" value='SEND MESSAGE' />
                 </form>
+                <div className='flex-1 flex flex-col gap-y-10 items-center justify-center'>
+                    <div className='flex flex-col items-center justify-center w-full py-3' style={{ backgroundColor: '#f4f6fa' }}>
+                        <i class="text-2xl rounded-full py-4 px-5 bg-[#f6e8de] fa-solid fa-location-pin"></i>
+                        <h4 className='text-xl font-bold mt-3'>ADDRESS</h4>
+                        <p className='mt-2'>Mohakhali Bazar, Dhaka 1212</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center w-full py-3' style={{ backgroundColor: '#f4f6fa' }}>
+                        <i class="text-2xl rounded-full py-4 px-4 bg-[#f6e8de]  fa-solid fa-envelope"></i>
+                        <h4 className='text-xl font-bold mt-3'>EMAIL</h4>
+                        <p className='mt-2'>example@gamil.com</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center w-full py-3' style={{ backgroundColor: '#f4f6fa' }}>
+                        <i class="text-2xl rounded-full py-4 px-4 bg-[#f6e8de]  fa-solid fa-phone"></i>
+                        <h4 className='text-xl font-bold mt-3'>PHONE</h4>
+                        <p className='mt-2'>(02) 323 23444</p>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SubBanner from '../components/SubBanner';
 import Head from 'next/head';
 import ContactForm from '../components/Contact/ContactForm';
+import Footer from '../components/Footer';
 
 const contact = () => {
     return (
@@ -9,6 +10,7 @@ const contact = () => {
             <Head><title>CONTACT US</title></Head>
             <SubBanner url={'https://templatekits.themewarrior.com/autodrive/wp-content/uploads/sites/42/2021/12/hero-contact.jpg'}>CONTAC US</SubBanner>
             <ContactForm></ContactForm>
+            <Footer></Footer>
         </main>
     );
 };
