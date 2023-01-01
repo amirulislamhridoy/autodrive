f2200f1d29c13ae552bf7c17fab3b821059232e4
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-01-02 03:53:29 +0600

    contact responsive

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -6,14 +6,14 @@ const ContactForm = () => {
         console.log(data)
     };
     return (
-        <section className='max-w-7xl mx-auto'>
+        <section className='max-w-7xl mx-2 xl:mx-auto'>
             <p className='text-[#bbbbbb] text-md sm:text-xl font-bold mt-10'>CONTACT US</p>
             <h2 className='text-2xl font-bold sm:text-4xl sm:mt-2 md:mt-4'>GET IN TOUCH</h2>
             <article className='mt-1 sm:mt-2 text-justify'>
                 Odio eu viverra tincidunt tristique ullamcorper blandit dipiscing nunc risus integer at elementum cursus. Lorem justo a felis elit amet.
             </article>
-            <div className='flex gap-x-12'>
-                <form className='p-7 bg-[#0a1931] mt-5 flex-1' onSubmit={handleSubmit(onSubmit)}>
+            <div className='sm:flex gap-x-12 my-4'>
+                <form className='p-7 bg-[#0a1931] mt-5 sm:w-7/12 xl:w-6/12' onSubmit={handleSubmit(onSubmit)}>
                     <input className='focus:outline-0 p-5 w-full mb-1' {...register("name", { required: true })} aria-invalid={errors.name ? "true" : "false"} placeholder='Your Name' />
                     {errors.name?.type === 'required' && <p style={{ color: '#da635d' }}>You should must write name.</p>}
                     <input className='focus:outline-0 p-5 w-full my-1' {...register("email", { required: true, pattern: /^\S+@\S+\.\S+$/ })} placeholder='example@gmail.com' />
@@ -46,20 +46,20 @@ const ContactForm = () => {
                     {errors?.message?.message && <p style={{ color: '#da635d' }}>{errors?.message?.message}</p>}
                     <input className='bg-[#ffc947] py-3 px-8 font-bold mt-2' type="submit" value='SEND MESSAGE' />
                 </form>
-                <div className='flex-1 flex flex-col gap-y-10 items-center justify-center'>
+                <div className='sm:w-4/12 xl:w-6/12 flex flex-col gap-y-4 xl:gap-y-10 items-center justify-center mt-5 sm:mt-0'>
                     <div className='flex flex-col items-center justify-center w-full py-3' style={{ backgroundColor: '#f4f6fa' }}>
-                        <i className="text-2xl rounded-full py-4 px-5 bg-[#f6e8de] fa-solid fa-location-pin"></i>
-                        <h4 className='text-xl font-bold mt-3'>ADDRESS</h4>
+                        <i className="text-xl md:text-2xl rounded-full py-4 px-5 bg-[#f6e8de] fa-solid fa-location-pin"></i>
+                        <h4 className='md:text-xl font-bold mt-3'>ADDRESS</h4>
                         <p className='mt-2'>Mohakhali Bazar, Dhaka 1212</p>
                     </div>
                     <div className='flex flex-col items-center justify-center w-full py-3' style={{ backgroundColor: '#f4f6fa' }}>
-                        <i className="text-2xl rounded-full py-4 px-4 bg-[#f6e8de]  fa-solid fa-envelope"></i>
-                        <h4 className='text-xl font-bold mt-3'>EMAIL</h4>
+                        <i className="text-xl md:text-2xl rounded-full py-4 px-4 bg-[#f6e8de]  fa-solid fa-envelope"></i>
+                        <h4 className='md:text-xl font-bold mt-3'>EMAIL</h4>
                         <p className='mt-2'>example@gamil.com</p>
                     </div>
                     <div className='flex flex-col items-center justify-center w-full py-3' style={{ backgroundColor: '#f4f6fa' }}>
-                        <i className="text-2xl rounded-full py-4 px-4 bg-[#f6e8de]  fa-solid fa-phone"></i>
-                        <h4 className='text-xl font-bold mt-3'>PHONE</h4>
+                        <i className="text-xl md:text-2xl rounded-full py-4 px-4 bg-[#f6e8de]  fa-solid fa-phone"></i>
+                        <h4 className='md:text-xl font-bold mt-3'>PHONE</h4>
                         <p className='mt-2'>(02) 323 23444</p>
                     </div>
                 </div>
--- pages/allCarData.js
+++ pages/allCarData.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import CarShortData from '../components/CarShortData';
 import Link from 'next/link';
+import Footer from '../components/Footer';
 
 const AllCarData = () => {
     const { isLoading, error, data: cars } = useQuery('repoData', () =>
@@ -19,7 +20,7 @@ const AllCarData = () => {
             </Head>
             <Navbar>All Car Data</Navbar>
             <section className='bg-[#f4f6fa]'>
-                <div className='max-w-7xl mx-auto pt-20 pb-16'>
+                <div className='max-w-7xl mx-auto pt-20 pb-5'>
                     <h1 className='text-5xl text-center font-bold'>WE OFFER BEST CAR</h1>
                     <p className='text-center mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                     <div className='mx-3 lg:mx-1 lg:grid grid-cols-2 gap-4 mt-8'>
@@ -28,6 +29,7 @@ const AllCarData = () => {
                     <div className='mt-5 text-center'><Link className='text-2xl border-b-2 border-[#ffc947] text-[#474FA0] hover:border-[#474FA0] pb-2' href='http://localhost:3000/' as=''>Back to home page &gt;</Link></div>
                 </div>
             </section>
+            <Footer></Footer>
         </main>
     );
 };
