c34927ba83b4c386677d3697c26dd7e685d70e32
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-01-02 03:37:08 +0600

    car data responsive

--- components/CarShortData.js
+++ components/CarShortData.js
@@ -3,21 +3,20 @@ import Link from 'next/link';
 
 const CarShortData = (props) => {
     const car = props.car
-    console.log(car)
     return (
-        <div className='flex gap-x-1 shadow-xl bg-white pr-3'>
-            <img className='w-6/12' src={car.img} alt=''></img>
-            <div className='self-center py-5'>
-                <h2 className='text-xl font-semibold'>Name: {car.name}</h2>
-                <p className='mt-1'>{car?.description?.slice(0, 74)}...</p>
-                <div className='mt-5'>
-                    <p>Seats: {car.seat}</p>
-                    <p>Gear: {car.gear}</p>
-                    <p>Air Condition: {car.airCondition === 'true' ? 'Yes' : "No"}</p>
+        <div className='my-5 lg:my-0 sm:flex shadow-xl bg-white sm:pr-3 rounded-md text-center sm:text-start px-5'>
+            <img className='w-6/12 mx-auto' src={car.img} alt=''></img>
+            <div className='self-center pb-5 sm:py-5'>
+                <h2 className='text-md sm:text-xl font-semibold '>Name: {car.name}</h2>
+                <p className='mt-1 '>{car?.description?.slice(0, 74)}...</p>
+                <div className='mt-3 lg:mt-2 xl:mt-5'>
+                    <p className=' '>Seats: {car.seat}</p>
+                    <p className=' '>Gear: {car.gear}</p>
+                    <p className=' '>Air Condition: {car.airCondition === 'true' ? 'Yes' : "No"}</p>
                 </div>
-                <div className='flex justify-between items-center'>
+                <div className='flex justify-between items-center mt-2 lg:mt-0'>
                     <Link className='border-b-2 border-[#ffc947] text-[#474FA0] hover:border-[#474FA0] pb-1 inline-block mt-3 font-bold' href='' as=''>View Details <span className='font-bold'>&gt;</span></Link>
-                    <button className='bg-[#ffc947] py-1.5 px-4 rounded'><span className='text-3xl'>${car.dayHire}</span><span className='font-bold'> / day</span></button>
+                    <button className='bg-[#ffc947] py-1.5  px-4 rounded'><span className='text-xl sm:text-2xl lg:text-xl xl:text-3xl'>${car.dayHire}</span><span className='font-bold'> / day</span></button>
                 </div>
             </div>
         </div>
--- pages/allCarData.js
+++ pages/allCarData.js
@@ -22,7 +22,7 @@ const AllCarData = () => {
                 <div className='max-w-7xl mx-auto pt-20 pb-16'>
                     <h1 className='text-5xl text-center font-bold'>WE OFFER BEST CAR</h1>
                     <p className='text-center mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                    <div className='grid grid-cols-2 gap-4 mt-8'>
+                    <div className='mx-3 lg:mx-1 lg:grid grid-cols-2 gap-4 mt-8'>
                         {cars?.map((car, i) => <CarShortData key={i} car={car}></CarShortData>)}
                     </div>
                     <div className='mt-5 text-center'><Link className='text-2xl border-b-2 border-[#ffc947] text-[#474FA0] hover:border-[#474FA0] pb-2' href='http://localhost:3000/' as=''>Back to home page &gt;</Link></div>
